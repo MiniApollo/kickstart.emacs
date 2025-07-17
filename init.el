3520d6a2808597205ed9940495b66ab8dd45fbdd
Mark Surmann <surmannmark@gmail.com>
2025-07-17 17:35:54 +0200

    Init:Evil: Disable evil-mode by default

--- init.el
+++ init.el
@@ -79,29 +79,6 @@
          )
   )
 
-(use-package evil
-  :init
-  (evil-mode)
-  :config
-  (evil-set-initial-state 'eat-mode 'insert) ;; Set initial state in eat terminal to insert mode
-  :custom
-  (evil-want-keybinding nil)    ;; Disable evil bindings in other modes (It's not consistent and not good)
-  (evil-want-C-u-scroll t)      ;; Set C-u to scroll up
-  (evil-want-C-i-jump nil)      ;; Disables C-i jump
-  (evil-undo-system 'undo-redo) ;; C-r to redo
-  (org-return-follows-link t)   ;; Sets RETURN key in org-mode to follow links
-  ;; Unmap keys in 'evil-maps. If not done, org-return-follows-link will not work
-  :bind (:map evil-motion-state-map
-              ("SPC" . nil)
-              ("RET" . nil)
-              ("TAB" . nil)))
-(use-package evil-collection
-  :after evil
-  :config
-  ;; Setting where to use evil-collection
-  (setq evil-collection-mode-list '(dired ibuffer magit corfu vertico consult info))
-  (evil-collection-init))
-
 (use-package general
   :config
   ;; (general-evil-setup) ;; <- evil
