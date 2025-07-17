d0d4948dbd0e74cd71dc9b5bf782c489074a97f5
Mark Surmann <surmannmark@gmail.com>
2025-07-17 22:16:52 +0200

    Init: Fix typos

--- init.el
+++ init.el
@@ -418,7 +418,7 @@
   (which-key-sort-order #'which-key-key-order-alpha) ;; Same as default, except single characters are sorted alphabetically
   (which-key-sort-uppercase-first nil)
   (which-key-add-column-padding 1) ;; Number of spaces to add to the left of each column
-  (which-key-min-display-lines 6)  ;; Increase the minimum lines to display, because the default is only 1
+  (which-key-min-display-lines 6)  ;; Increase the minimum lines to display because the default is only 1
   (which-key-idle-delay 0.8)       ;; Set the time delay (in seconds) for the which-key popup to appear
   (which-key-max-description-length 25)
   (which-key-allow-imprecise-window-fit nil)) ;; Fixes which-key window slipping out in Emacs Daemon
