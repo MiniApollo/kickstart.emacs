faa291c295d4c93179449a337c819cb97183424b
Mark Surmann <surmannmark@gmail.com>
2026-02-11 21:33:25 +0100

    Update multi file example

--- lisp/start-multiFileExample.el
+++ lisp/start-multiFileExample.el
@@ -1,5 +1,10 @@
-(defun start/hello()
+;;; start-multiFileExample.el --- Example how to write multi file config -*- lexical-binding: t -*-
+
+;;; Code:
+
+(defun start/hello ()
   "Say hello from another file"
   (message "Hello from another file"))
 
 (provide 'start-multiFileExample)
+;;; start-multiFileExample.el ends here
